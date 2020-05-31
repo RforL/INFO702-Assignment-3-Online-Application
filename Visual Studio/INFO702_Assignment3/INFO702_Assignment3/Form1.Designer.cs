b11e0884842a83cb0d639cb9f639eb3527be6cef
RforL <RforL@users.noreply.github.com>
2020-05-31 21:51:48 +1200

    CyberSecurity

---
+++ Visual Studio/INFO702_Assignment3/INFO702_Assignment3/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace INFO702_Assignment3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.leftBar = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.resultsBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.leftBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // leftBar
+            // 
+            this.leftBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(18)))), ((int)(((byte)(41)))));
+            this.leftBar.Controls.Add(this.pictureBox1);
+            this.leftBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftBar.Location = new System.Drawing.Point(0, 0);
+            this.leftBar.Name = "leftBar";
+            this.leftBar.Size = new System.Drawing.Size(509, 720);
+            this.leftBar.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::INFO702_Assignment3.Properties.Resources.logo_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(141, 173);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 238);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat Light", 40F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(525, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 92);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat Light", 30F);
+            this.label2.Location = new System.Drawing.Point(529, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 69);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email:";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.emailTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.emailTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.emailTextBox.Font = new System.Drawing.Font("Montserrat", 20F);
+            this.emailTextBox.Location = new System.Drawing.Point(541, 173);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(813, 48);
+            this.emailTextBox.TabIndex = 1;
+            // 
+            // resultsBox
+            // 
+            this.resultsBox.BackColor = System.Drawing.Color.White;
+            this.resultsBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.resultsBox.Font = new System.Drawing.Font("Montserrat", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultsBox.Location = new System.Drawing.Point(541, 243);
+            this.resultsBox.Multiline = true;
+            this.resultsBox.Name = "resultsBox";
+            this.resultsBox.ReadOnly = true;
+            this.resultsBox.Size = new System.Drawing.Size(813, 383);
+            this.resultsBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Montserrat", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1166, 632);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 76);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1366, 720);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultsBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.leftBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.leftBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel leftBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox resultsBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Visual Studio/INFO702_Assignment3/INFO702_Assignment3/Form1.cs
@@ -0,0 +1,89 @@
+﻿using AtleX.HaveIBeenPwned;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INFO702_Assignment3
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ///Form Elements
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+
+        //Variables
+        private bool mouseDown;
+        private Point lastLocation;
+
+        //Mouse Down Event
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        //Mouse Move Event
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        //Mouse Up Event
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resultsBox.Clear();
+
+            // Create the client
+            var settings = new HaveIBeenPwnedClientSettings()
+            {
+                ApiKey = "00b9d0e69f9345799eda0ea03cdd9709", // Get one from https://haveibeenpwned.com/API/Key. Necessary for getting the breaches and pastes an account is in.
+                ApplicationName = "HaveIBeenHacked",
+            };
+
+            using (var client = new HaveIBeenPwnedClient(settings))
+            {
+                if (emailTextBox.Text == null)
+                {
+                    resultsBox.Text = "Please enter an email";
+                }
+
+                // Get the breaches for an account. This returns a collection of breaches with their 
+                // name. Use the response from GetAllBreachesAsync() to find the corresponding details 
+                // by name
+                var breaches = await client.GetBreachesAsync(emailTextBox.Text); //Rquires an API key
+
+                foreach (var breach in breaches)
+                {
+                    resultsBox.AppendText("Breach Name: " + breach.Name + "\r\n");
+                }
+
+
+            }
+        }
+
+        ///Buttons
+    }
+}
